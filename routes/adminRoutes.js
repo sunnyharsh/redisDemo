dfddbc47b5ae6e5710244d58e3364554ce6d72d7
sunnyharsh <sunnyharshsunny12345@gmail.com>
2022-02-13 21:11:23 +0530

    add notes and also add expiry time of key

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,9 +3,9 @@ const axios= require('axios')
 const setAndGetCache=require("../service/redis.service").setAndGetCache;
 
 const adminRoutes=express.Router()
-
+const EXPIRY_DEFAULT=20
 adminRoutes.get('/getData',async (req, res)=>{
-    const getData= await setAndGetCache('userData',async ()=>{
+    const getData= await setAndGetCache('userData', EXPIRY_DEFAULT ,async ()=>{
     const response=await axios.get("https://randomuser.me/api/");
         return response.data;
     })
